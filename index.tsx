6875d13a4cbc00814286c3fa2c61f8eae5646ef1
pusan112 <128443630+pusan112@users.noreply.github.com>
2025-12-30 23:27:31 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App"; // 引入主应用组件
-import { AuthProvider } from "./AuthProvider"; // 引入 AuthProvider（从 src/AuthProvider.tsx）
+import { AuthProvider } from './src/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
