e292308ef5d17a562a95391105fe7c0d8e419666
pusan112 <128443630+pusan112@users.noreply.github.com>
2025-12-31 00:01:54 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 // src/index.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // 引入主应用组件
-import { AuthProvider } from './AuthProvider';
+import App from "./App"; 
+import { AuthProvider } from "./AuthProvider";  // 与文件一致
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>  {/* 用 AuthProvider 包裹整个应用 */}
+    <AuthProvider>
       <App />
     </AuthProvider>
   </React.StrictMode>
